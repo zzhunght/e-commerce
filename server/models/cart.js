c587b25051aa2db1ba0e7183ffa2d24942eee2c7
lê hùng <tinhyeucuatui2002@gmail.com>
2021-12-23 16:54:31 +0700

    init-cart-schema

---
+++ server/models/cart.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const CartSchema = new Schema({
+    user:{
+        type:Schema.Types.ObjectId,
+        ref:'user',
+        require:true
+    },
+    createdAt:{
+        type:Date,
+        default:Date.now
+    },
+    updatedAt:{
+        type:Date,
+        default:Date.now
+    },
+    products:[
+       {    
+           skus:{
+               size:{
+                   type:String,
+                   require:true
+               },
+               price:{
+                   type:Number,
+                   require:true
+               }
+           },
+           productId:{
+               type:Schema.Types.ObjectId,
+               ref:'product',
+               require:true
+           },
+           name:{
+               type:String,
+               require:true
+           },
+           quantity:{
+               type:Number,
+               default:1,
+               require:true
+           },
+           
+       }
+    ]
+})
+
+
+module.exports = mongoose.model('cart',CartSchema)
